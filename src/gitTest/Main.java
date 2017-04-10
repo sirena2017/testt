cc1894bc12b1ec5e41de96d65672d5d9c1427881
namak <mazhari44@gmail.com>
2017-04-10 17:29:13 +0430

    get input added

--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -1,7 +1,14 @@
 package gitTest;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String [] args){
+		Scanner scan=new Scanner(System.in);
+		System.out.println("please enter a number");
+		
+		InOutManag io=new InOutManag();
+		io.input(scan.nextInt());
 		
 	}
 
